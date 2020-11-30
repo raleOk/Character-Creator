8a892dbd394d47574ef7892af1364f866315e4e1
Rastko Zivkovic <rale.pablito@gmail.com>
2020-11-30 20:37:48 +0100

    Second commit

--- app.js
+++ app.js
@@ -1,73 +1,104 @@
-const Model = (function () {
+const Model = (() => {
     
     class Hero {
-        constructor(name, level) {
+        constructor(id, name, level) {
+            this.id = id;
             this.name = name;
             this.level = level;
         }
     }
     
     class Warrior extends Hero {
-        constructor(name, level, weapon) {
-            super(name, level);
+        constructor(id, name, level, weapon) {
+            super(id, name, level);
             this.weapon = weapon;
         }
     }
 
     class Mage extends Hero {
-        constructor(name, level, spell) {
-            super(name, level);
+        constructor(id, name, level, spell) {
+            super(id, name, level);
             this.spell = spell;
         }
     }
 
-    const addHero = (name, level, weapon, hClass) => {
+    const addHero = (id, name, level, weapon, hClass) => {
          let hero;
         if (hClass.value === 'mage') { 
-            hero = new Mage (name.value, Number(level.value), weapon.value);
+            hero = new Mage (id, name.value, Number(level.value), weapon.value);
             data.mages.push(hero);
         } else {
-            hero = new Warrior (name.value, Number(level.value), weapon.value);
+            hero = new Warrior (id, name.value, Number(level.value), weapon.value);
             data.warriors.push(hero);
         }     
+    }
+
+    const compareLvls = () => {
+        const bestHeroArr = [];
+
+        if (data.warriors.length > 0) {
+            bestHeroArr.push(data.warriors.reduce((max, hero) => max.level > hero.level ? max : hero));
+        }
+
+        if (data.mages.length > 0) {
+            bestHeroArr.push(data.mages.reduce((max, hero) => max.level > hero.level ? max : hero));
+        }
+        
+        return bestHeroArr.reduce((max, hero) => max.level > hero.level ? max : hero);
+    }
+
+    const uniqueID = () => {
+        return Math.floor(Math.random() * Date.now())
     }
 
     const data = {
         warriors: [],
         mages: []
-    };
+    }
         
     return {
         data,
-        addHero
+        addHero,
+        uniqueID,
+        compareLvls
     }
 
 })();
 
-const View = (function () {
 
-    function displayWarrior(name, level, weapon, list) {
+const View = (() => {
 
-        const warriorElement = document.createElement('div');
-        warriorElement.innerHTML = `<p>Name:${name}<span> Level:${level}</span> Weapon:${weapon}</p>`;     
+    const displayWarrior = (name, level, weapon, list, id) => {
+
+        const warriorElement = document.createElement('p');
+        warriorElement.setAttribute('id', id);
+        warriorElement.innerHTML = `Name:${name} <span>Level:${level}</span> Weapon:${weapon}`;     
         list.appendChild(warriorElement);
     } 
 
-    function displayMage(name, level, spell, list) {
+    const displayMage = (name, level, spell, list, id) => {
 
-        const mageElement = document.createElement('div');
-        mageElement.innerHTML = `<p>Name:${name}<span> Level:${level}</span> Spell:${spell}</p>`;
+        const mageElement = document.createElement('p');
+        mageElement.setAttribute('id', id);
+        mageElement.innerHTML = `Name:${name} <span>Level:${level}</span> Spell:${spell}`;
         list.appendChild(mageElement);
+    }
+
+    const displayWinner = (obj) => {
+        const winnerDiv = document.getElementById(obj.id);
+        winnerDiv.classList.add('winner');
     }
 
     return { 
         displayWarrior,
-        displayMage
+        displayMage,
+        displayWinner
     }
 
 })();
 
-const Controller = (function (Model, View) {
+
+const Controller = ((Model, View) => {
 
     const heroName = document.getElementById('heroName');
     const heroLevel = document.getElementById('heroLevel');
@@ -81,45 +112,26 @@ const Controller = (function (Model, View) {
     const fightBtn = document.getElementById('fightBtn');
 
     createCharacterBtn.addEventListener('click', init);
-    fightBtn.addEventListener('click', compareLvls);
+    fightBtn.addEventListener('click', () => {
+        y = Model.compareLvls();
+        View.displayWinner(y);
+    })
 
     function init() {
-        Model.addHero(heroName, heroLevel, heroWeapon, heroClass);
-        init2()
-    };
-
+        Model.addHero(Model.uniqueID(), heroName, heroLevel, heroWeapon, heroClass);
+        init2();
+    }
 
     function init2() {
         fightBtn.style.display = "inline-block"
         warriorList.innerHTML = '';
         mageList.innerHTML = '';
         for (let x of Model.data.warriors) {
-            View.displayWarrior(x.name, x.level, x.weapon, warriorList);
+            View.displayWarrior(x.name, x.level, x.weapon, warriorList, x.id);
         }
         for (let x of Model.data.mages) {
-            View.displayMage(x.name, x.level, x.spell, mageList);
+            View.displayMage(x.name, x.level, x.spell, mageList, x.id);
         }
     }
-
-    function compareLvls() {
-        let bestHeroArr=[];
-        let bestHero;
-        let champ;
-                                                                        
-        bestHeroArr.push(Math.max.apply(null, Model.data.warriors.map((o) => {return o.level})));
-        bestHeroArr.push(Math.max.apply(null, Model.data.mages.map((o) => {return o.level})));
-
-        if (bestHeroArr[0]>bestHeroArr[1]) {
-            bestHero = bestHeroArr[0];
-            champ = Model.data.warriors.find(function(o){return o.level == bestHero})
-            return console.log(`${champ.name} used ${champ.weapon} and won!`);
-        } else {
-            bestHero = bestHeroArr[1];
-            champ = Model.data.mages.find(function(o){return o.level == bestHero})
-            return console.log(`${champ.name} used ${champ.spell} and won!`);
-        }
-    }
-
-})(Model, View);
-
-
+    
+}) (Model, View);
